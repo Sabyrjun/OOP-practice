3a2b05d2c997362609c65751e8f460fa0685fc22
Retr0 <sabyr1232501@gmail.com>
2025-12-16 14:09:49 +0500

    commit

--- src/Booking.java
+++ src/Booking.java
@@ -5,6 +5,26 @@ public class Booking {
         private double price;
         private boolean avaliable;
 
+        public Room(int number, String type, double price, boolean avaliable){
+            this.number = number;
+            this.type = type;
+            this.price = price;
+            this.avaliable = avaliable;
+        }
+        public int getNumber() {return number;}
+        public void setNumber(int number){this.number;}
+
+        public String getType(){return type;}
+        public double getPrice() {return price;}
+
+        public boolean isAvaliable() {return avaliable;}
+        public void setAvaliable(boolean avaliable){this.avaliable = avaliable}
+    }
+
+
+
+
+
     }
 
 
