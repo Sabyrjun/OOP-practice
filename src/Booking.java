d843753dcb71524052555079e65401539c06ec48
Retr0 <sabyr1232501@gmail.com>
2025-12-16 12:55:01 +0500

    commit

---
+++ src/Booking.java
@@ -0,0 +1,12 @@
+public class Booking {
+    class Room{
+        private int number;
+        private String type;
+        private double price;
+        private boolean avaliable;
+
+    }
+
+
+
+}
